a20ca22979f6894f5f200255947607d5241cbd18
Oscar <dias_oscar@hotmail.com>
2021-02-25 08:37:58 -0300

    Created surveys routes

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,33 +1,33 @@
-import { Request, Response } from "express"
-import { getRepository } from "typeorm";
-import { User } from "../models/User";
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 class UserController {
-    async create(request: Request, response: Response) {
-        const { name, email } = request.body;
-        
-        const userRepository = getRepository(User);
+  async create(request: Request, response: Response) {
+    const { name, email } = request.body;
 
-        const userAlreadyExists = await userRepository.findOne({
-            email
-        });
+    const userRepository = getCustomRepository(UsersRepository);
 
-        if (userAlreadyExists) {
-            return response.status(400).json({
-                error: "User already exists!"
-            })
-        }
+    const userAlreadyExists = await userRepository.findOne({
+      email
+    });
 
-        const user = userRepository.create({
-            name,
-            email
-        })
-
-        await userRepository.save(user);
-
-        return response.json(user);
-        
+    if (userAlreadyExists) {
+      return response.status(400).json({
+        error: "User already exists!"
+      })
     }
+
+    const user = userRepository.create({
+      name,
+      email
+    })
+
+    await userRepository.save(user);
+
+    return response.json(user);
+
+  }
 }
 
-export { UserController }
+export { UserController };
--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,18 @@
 import { Router } from 'express';
+import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
 
 const router = Router();
 
 const userController = new UserController();
+const surveysController = new SurveysController();
 
 router.get("/", (_request, response) => {
     return response.status(200).json({message: "NLW-04 - NodeJs Server"})
 })
 
 router.post("/users", userController.create);
+router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
 
 export { router };
